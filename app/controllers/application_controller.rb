07c9821e0f52a1582b0dafb49e0674e2b75a54b2
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-09-14 10:37:21 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "d5b08b5689ee04a7c1f7d651f73cce129badfe2b960364ce59e6d4a16d86e7b83407563a1d8c43efe2cc17ad1e96b74d8d6d8dc230f1a613da09cd636e0904f9"
   end
 
+  get '/' do 
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user # !! returns truthy or falsey value specifically
+    end
+
+    def current_user
+      # binding.pry
+      # ||= sets value of variable on the left side to the value listed on the right side unless the variable on the left already has a value, then that value will stay the same and not get over ridden by what's to the right of the ||= operator
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+
+  def slug
+    self.username.parameterize
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|x| x.slug == slug}
+  end
 end
